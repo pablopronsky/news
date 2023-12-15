891312983216e3cc1892bbfe590b50f5c1620ff9
pablopronsky <pronskypablo@gmail.com>
2023-12-15 19:17:10 -0300

    new remotes

---
+++ lib/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:que_me_pongo/core/resources/data_state.dart';
+import 'package:que_me_pongo/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:que_me_pongo/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+
+import '../../../../domain/usecases/get_article.dart';
+
+class RemoteArticlesBloc extends Bloc<RemoteArticleEvent,RemoteArticleState>{
+
+  final GetArticleUseCase _getArticleUseCase;
+  RemoteArticlesBloc(this._getArticleUseCase) : super(const RemoteArticlesLoading()){
+    on <GetArticles> (onGetArticles);
+  }
+
+  Future<void> onGetArticles(GetArticles event, Emitter<RemoteArticleState> emit) async {
+    final dataState = await _getArticleUseCase();
+
+    if(dataState is DataSuccess && dataState.data!.isNotEmpty){
+      emit(
+        RemoteArticlesDone(dataState.data!)
+      );
+    }
+    if(dataState is DataFailed){
+      emit(
+        RemoteArticlesError(dataState.error!)
+      );
+    }
+  }
+}

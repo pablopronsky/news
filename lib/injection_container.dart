c8cb250a08741c5e07b8ad479ba3502ad664c7fb
pablopronsky <pronskypablo@gmail.com>
2023-12-16 15:57:11 -0300

    added blocs an singleton to instance class

---
+++ lib/injection_container.dart
@@ -0,0 +1,21 @@
+import 'package:get_it/get_it.dart';
+import 'package:que_me_pongo/features/daily_news/data/data_sources/remote/news_api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:que_me_pongo/features/daily_news/data/repository/article_repository_implementation.dart';
+import 'package:que_me_pongo/features/daily_news/domain/repository/article_repository.dart';
+import 'package:que_me_pongo/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initializeDependencies() async{
+  sl.registerSingleton<Dio>(Dio());
+  sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
+  sl.registerSingleton<ArticleRepository>(
+    ArticleRepositoryImplementation(sl())
+  );
+
+  // blocs
+  sl.registerFactory<RemoteArticlesBloc>(
+      () => RemoteArticlesBloc(sl())
+  );
+}
